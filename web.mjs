c8f5e7d748fb2c7e1713d216ce7769723ac3fc89
Lyumih <Lyumih@users.noreply.github.com>
2025-10-31 07:20:23 +0000

    Deploying to gh-pages from @ Lyumih/mvc@786bf0fce1f3c4a9815ab65d7922571498946d6a 🚀

--- web.mjs
+++ web.mjs
@@ -4049,6 +4049,7 @@ var $;
             Body_content: {
                 padding: $mol_gap.block,
                 minHeight: 0,
+                minWidth: 0,
                 flex: {
                     direction: 'column',
                     shrink: 1,
